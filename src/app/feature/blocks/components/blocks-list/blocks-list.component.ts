892d72307f8b14f51d4fb6dd835d2983b3d853d7
Tverone003 <veroneteffo@gmail.com>
2023-11-27 11:03:16 +0100

    remove .github on release

--- src/app/feature/blocks/components/blocks-list/blocks-list.component.ts
+++ src/app/feature/blocks/components/blocks-list/blocks-list.component.ts
@@ -58,7 +58,7 @@ export class BlocksListComponent implements OnInit, OnDestroy {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
-    return `Le ${day}-${month}-${year} à ${hours}:${minutes}:${seconds}`;
+    return `${day}-${month}-${year} à ${hours}:${minutes}:${seconds}`;
   }
 
 
